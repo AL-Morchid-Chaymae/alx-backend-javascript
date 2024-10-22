c874a57a3761d1950c747814a0a8e92b76e3e3b7
AL-Morchid-Chaymae <almorchidchaymae1@gmail.com>
2024-10-22 19:53:05 +0000

    task2

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,32 +1,11 @@
 export default class Currency {
-  constructor(code, name) {
-    this.code = code;
-    this.name = name;
-  }
-
-  displayFullCurrency() {
-    return `${this._name} (${this._code})`;
-  }
-
-  get code() {
-    return this._code;
-  }
-
-  set code(code) {
-    if ((typeof code !== 'string') && !(code instanceof String)) {
-      throw new TypeError('Code must be a string');
+    constructor() {
+        this._name = 'Dollars';
+        this._code = '$';
     }
-    this._code = code;
-  }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    if ((typeof name !== 'string') && !(name instanceof String)) {
-      throw new TypeError('Name must be a string');
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`; // Retourner le format souhaité
     }
-    this._name = name;
-  }
 }
+
